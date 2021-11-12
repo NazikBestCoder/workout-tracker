661bd8433a4285200ba22542026ef446cd1a1b2c
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-11-12 00:15:38 -0500

    fixed localhost

---
+++ controllers/routes/apiRoutes.js
@@ -0,0 +1,90 @@
+const Workout = require("../../models/Workout");
+const express = require("express");
+const router = express.Router();
+// const { get } = require("http");
+
+
+
+router.post("/api/workouts", ({ body }, res) => {
+    try {
+      Workout.create(body).then((workout) => {
+        console.log(workout);
+        res.json(workout);
+      });
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(400).json(err);
+    }
+  });
+  
+ 
+  router.get("/api/workouts", async (req, res) => {
+    try {
+     
+  
+      const workoutData = await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: '$exercises.duration'
+            },
+          }
+        }
+      ])
+      res.json(workoutData);
+  
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
+  });
+  
+  router.put("/api/workouts/:id", async ({ params, body }, res) => {
+    try {
+      const results = await Workout.findByIdAndUpdate(
+        params.id,
+        {
+          $push: { exercises: body },
+        },
+        { new: true, runValidators: true }
+      );
+      res.json(results);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
+  });
+ 
+  router.get("/api/workouts/range", async (req, res) => {
+    try {
+      const workoutData = await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: '$exercises.duration'
+            }
+          }
+        }
+      ])
+      .sort({_id: -1})
+      .limit(7)
+      res.json(workoutData);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
+  });
+  
+  router.post("/api/workouts/range", async (req, res) => {
+    try {
+      Workout.create({}).then((workout) => {
+        res.json(workout);
+      });
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500).json(err);
+    }
+  });
+  
+  module.exports = router;
+  
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./controllers/routes/apiRoutes");
+const htmlRoutes = require("./controllers/routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3005;
 const app = express();
@@ -11,7 +11,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-    useNewUrlParser: true });
+    useNewUrlParser: true 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+ });
+ 
 
 app.use(apiRoutes);
 app.use(htmlRoutes);
